ef895cdb2c625a2010d6e34f888b3f357c8ac32e
William Decker <williamd@msidata.com>
2016-10-04 13:36:21 -0500

    Add NUnit project

---
+++ FirstFloor.Xcc.NUnit/PageTests.cs
@@ -0,0 +1,140 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using NUnit.Framework;
+
+namespace FirstFloor.Xcc.NUnit
+{
+    [TestFixture]
+    public class PageTests
+    {
+        [Test]
+        public void TestMyPageWin81Debug()
+        {
+            TestXaml("WINDOWS_APP;DEBUG", false, "MyPage.xaml", "MyPage.Win81.Debug.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWin81DebugRemoveIgnorableContent()
+        {
+            TestXaml("WINDOWS_APP;DEBUG", true, "MyPage.xaml", "MyPage.Win81.Debug.RemoveIgnorableContent.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWP81Debug()
+        {
+            TestXaml("WINDOWS_PHONE_APP;DEBUG", false, "MyPage.xaml", "MyPage.WP81.Debug.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWP81DebugRemoveIgnorableContent()
+        {
+            TestXaml("WINDOWS_PHONE_APP;DEBUG", true, "MyPage.xaml", "MyPage.WP81.Debug.RemoveIgnorableContent.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWin81Release()
+        {
+            TestXaml("WINDOWS_APP;!DEBUG", false, "MyPage.xaml", "MyPage.Win81.Release.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWin81ReleaseRemoveIgnorableContent()
+        {
+            TestXaml("WINDOWS_APP;!DEBUG", true, "MyPage.xaml", "MyPage.Win81.Release.RemoveIgnorableContent.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWP81Release()
+        {
+            TestXaml("WINDOWS_PHONE_APP;!DEBUG", false, "MyPage.xaml", "MyPage.WP81.Release.expected.xaml");
+        }
+
+        [Test]
+        public void TestMyPageWP81ReleaseRemoveIgnorableContent()
+        {
+            TestXaml("WINDOWS_PHONE_APP;!DEBUG", true, "MyPage.xaml", "MyPage.WP81.Release.RemoveIgnorableContent.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageAndroid()
+        {
+            TestXaml("__ANDROID__", false, "XamarinContentPage.xaml", "XamarinContentPage.android.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageAndroidRemoveIgnorableContent()
+        {
+            TestXaml("__ANDROID__", true, "XamarinContentPage.xaml", "XamarinContentPage.android.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageiOs()
+        {
+            TestXaml("__IOS__", false, "XamarinContentPage.xaml", "XamarinContentPage.ios.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageiOsRemoveIgnorableContent()
+        {
+            TestXaml("__IOS__", true, "XamarinContentPage.xaml", "XamarinContentPage.ios.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageWP()
+        {
+            TestXaml("WINDOWS_PHONE", false, "XamarinContentPage.xaml", "XamarinContentPage.wp.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageWPRemoveIgnorableContent()
+        {
+            TestXaml("WINDOWS_PHONE", true, "XamarinContentPage.xaml", "XamarinContentPage.wp.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageNoSymbols()
+        {
+            TestXaml(null, false, "XamarinContentPage.xaml", "XamarinContentPage.nosymbols.expected.xaml");
+        }
+
+        [Test]
+        public void TestXamarinContentPageNoSymbolsRemoveIgnorableContent()
+        {
+            TestXaml(null, true, "XamarinContentPage.xaml", "XamarinContentPage.nosymbols.expected.xaml");
+        }
+
+        private static void TestXaml(string symbols, bool removeIgnorableContent, string xamlName, string expectedXamlName)
+        {
+            var preprocessor = new XamlPreprocessor(symbols, removeIgnorableContent);
+            var xaml = LoadXamlPage(xamlName);
+            var expected = LoadXamlPage(expectedXamlName);
+            var result = preprocessor.ProcessXaml(xaml);
+
+            // perform char-by-char comparison, raise error with index info if mismatch
+            var lineNumber = 1;
+            for (var i = 0; i < expected.Length && i < result.Length; i++) {
+                if (expected[i] != result[i]) {
+                    Assert.Fail("Character mismatch at index {0} (line number: {1}. Expected: {2} ({3}), actual: {4} ({5})", i, lineNumber, result[i], (int)result[i], expected[i], (int)expected[i]);
+                }
+                if (result[i] == '\n') {
+                    lineNumber++;
+                }
+            }
+
+            // still fail if one string is substring of the other 
+            Assert.AreEqual(expected, result);
+        }
+
+        private static string LoadXamlPage(string pageName)
+        {
+            var fullName = string.Format(CultureInfo.InvariantCulture, "FirstFloor.Xcc.NUnit.Xaml.{0}", pageName);
+
+            using (var stream = typeof(PageTests).Assembly.GetManifestResourceStream(fullName)) {
+                using (var reader = new StreamReader(stream)) {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ FirstFloor.Xcc.NUnit/XmlTests.cs
@@ -0,0 +1,154 @@
+﻿using NUnit.Framework;
+
+namespace FirstFloor.Xcc.NUnit
+{
+    [TestFixture]
+    public class XmlTests
+    {
+        [Test]
+        public void TestInputShouldNotChange()
+        {
+            var xml =  @"
+<Test xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <d:Element />
+</Test>";
+            TestXml(null, false, xml, xml);
+        }
+
+        [Test]
+        public void TestRemoveIgnorableContent()
+        {
+            var xml = @"
+<Test xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <d:Element />
+</Test>";
+            var expected = @"
+<Test>
+
+
+  <Element />
+  
+</Test>";
+
+            TestXml(null, true, xml, expected);
+        }
+
+        [Test]
+        public void TestRootNotInDefaultNamespace()
+        {
+            var xml = @"
+<foo:Test xmlns=""http://xcc.com""
+      xmlns:debug=""condition:DEBUG""
+      xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns:foo=""http://foo""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <debug:Element />
+</foo:Test>";
+            var expected = @"
+<foo:Test xmlns=""http://xcc.com"" xmlns:d=""http://schemas.microsoft.com/expression/blend/2008"" xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006"" xmlns:foo=""http://foo"" mc:Ignorable=""d"">
+
+
+
+
+
+  <Element d:Foo=""42"" />
+  <Element />
+</foo:Test>";
+
+            TestXml("DEBUG", false, xml, expected);
+        }
+
+        [Test]
+        public void TestRootNotInDefaultNamespaceWithNoDefaultNamespace()
+        {
+            var xml = @"
+<foo:Test xmlns:debug=""condition:DEBUG""
+      xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns:foo=""http://foo""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <debug:Element />
+</foo:Test>";
+            var expected = @"
+<foo:Test xmlns:d=""http://schemas.microsoft.com/expression/blend/2008"" xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006"" xmlns:foo=""http://foo"" mc:Ignorable=""d"">
+
+
+
+
+  <Element d:Foo=""42"" />
+  <foo:Element />
+</foo:Test>";
+
+            TestXml("DEBUG", false, xml, expected);
+        }
+
+        [Test]
+        public void TestRootNotInDefaultNamespaceWithDefaultNamespaceRemoveIgnorableContent()
+        {
+            var xml = @"
+<foo:Test xmlns=""http://xcc.com""
+      xmlns:debug=""condition:DEBUG""
+      xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns:foo=""http://foo""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <debug:Element />
+</foo:Test>";
+            var expected = @"
+<foo:Test xmlns=""http://xcc.com"" xmlns:foo=""http://foo"">
+
+
+
+
+
+  <Element />
+  <Element />
+</foo:Test>";
+
+            TestXml("DEBUG", true, xml, expected);
+        }
+
+        [Test]
+        public void TestRootNotInDefaultNamespaceWithNoDefaultNamespaceRemoveIgnorableContent()
+        {
+            var xml = @"
+<foo:Test xmlns:debug=""condition:DEBUG""
+      xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns:foo=""http://foo""
+      mc:Ignorable=""d"">
+  <Element d:Foo=""42"" />
+  <debug:Element />
+</foo:Test>";
+            var expected = @"
+<foo:Test xmlns:foo=""http://foo"">
+
+
+
+
+  <Element />
+  <foo:Element />
+</foo:Test>";
+
+            TestXml("DEBUG", true, xml, expected);
+        }
+
+        private static void TestXml(string symbols, bool removeIgnorableContent, string xaml, string expected)
+        {
+            var preprocessor = new XamlPreprocessor(symbols, removeIgnorableContent);
+            var result = preprocessor.ProcessXaml(xaml);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
